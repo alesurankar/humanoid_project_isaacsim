0c557878d374fdeb2b620176a4e504c236827c13
alesurankar <ale.urankar@gmail.com>
2026-05-14 09:10:08 +0200

    remove smooth interpolation

--- scripts/live_json_player.py
+++ scripts/live_json_player.py
@@ -8,15 +8,18 @@ import omni.timeline
 from isaacsim.core.prims import SingleArticulation
 from joint_map import joint_map
 
+# TIMELINE
 timeline = omni.timeline.get_timeline_interface()
 
 # START SIMULATION
 if not timeline.is_playing():
     timeline.play()
 
+# ROBOT
 robot = SingleArticulation("/s2_v1/base_link")
 robot.initialize()
 
+# JSON FILE
 JSON_PATH = (
     r"C:\Users\proje\Desktop\alesurankar\scripts\humanoid_project_isaacsim\data\motion.json"
 )
@@ -29,9 +32,11 @@ async def live_json_motion():
     while True:
 
         try:
+
+            # CHECK FILE MODIFICATION TIME
             modified = os.path.getmtime(JSON_PATH)
 
-            # FILE CHANGED
+            # ONLY UPDATE WHEN FILE CHANGES
             if modified != last_modified:
 
                 last_modified = modified
@@ -42,46 +47,36 @@ async def live_json_motion():
                 with open(JSON_PATH, "r") as f:
                     motion = json.load(f)
 
+                # CURRENT ROBOT POSE
                 current_positions = robot.get_joint_positions()
+
+                # COPY CURRENT POSE
                 target_positions = current_positions.copy()
 
-                # READ JOINTS FROM JSON
+                # JSON JOINT DATA
                 joints = motion["joints"]
 
-                # CONVERT NAMES -> INDICES
+                # APPLY JSON VALUES
                 for joint_name, value in joints.items():
-
                     if joint_name in joint_map:
-
                         joint_index = joint_map[joint_name]
-
                         target_positions[joint_index] = value
 
-                # SMOOTH INTERPOLATION
-                steps = 120
+                    else:
+                        print(f"UNKNOWN JOINT: {joint_name}")
 
-                for step in range(steps):
-
-                    alpha = (step + 1) / steps
-
-                    interpolated = (
-                        current_positions * (1 - alpha)
-                        + target_positions * alpha
-                    )
-
-                    robot.set_joint_positions(interpolated)
-
-                    await omni.kit.app.get_app().next_update_async()
+                # SEND TARGETS TO ROBOT
+                robot.set_joint_position_targets(target_positions)
 
         except Exception as e:
 
             print("ERROR:", e)
 
-        # CHECK EVERY FRAME
+        # WAIT ONE FRAME
         await omni.kit.app.get_app().next_update_async()
 
 
-# RUN
+# START ASYNC TASK
 asyncio.ensure_future(
     live_json_motion()
 )
