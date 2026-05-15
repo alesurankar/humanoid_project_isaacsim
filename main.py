a47e50a5d4978e121e9fcbde89679c91d7217a2f
alesurankar <ale.urankar@gmail.com>
2026-05-15 13:22:23 +0200

    fix main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import omni.timeline
 from isaacsim.core.prims import SingleArticulation
 from isaacsim.core.utils.stage import open_stage
 
-from players.motion_player import (
-    live_json_motion
-)
+# from players.motion_player import (
+#     live_json_motion
+# )
 
 import players.udp_receiver as udp_receiver
 
@@ -41,30 +41,55 @@ robot.initialize()
 print("Robot initialized!")
 
 # START JSON MOTION PLAYER
-asyncio.ensure_future(
-    live_json_motion(robot)
-)
+# asyncio.ensure_future(
+#     live_json_motion(robot)
+# )
 
 print("Motion player started!")
+
+# TRACK PHYSICS STATE
+robot_ready = True
 
 # MAIN LOOP
 while simulation_app.is_running():
 
     simulation_app.update()
+
+    # RECEIVE UDP
     udp_receiver.udp_spin_once()
 
-    if udp_receiver.latest_joint_positions is not None:
+    # HANDLE STOP/PLAY
+    if timeline.is_playing():
 
-        if timeline.is_playing() and robot.handles_initialized:
+        # Physics restarted
+        if not robot_ready:
 
             try:
-                robot.set_joint_positions(
-                    udp_receiver.latest_joint_positions
-                )
-
-                print("APPLIED")
+                robot.initialize()
+                robot_ready = True
+                print("ROBOT REINITIALIZED")
 
             except Exception as e:
-                print("SET JOINT ERROR:", e)
+                print("ROBOT INIT ERROR:", e)
+
+    else:
+        # Physics stopped
+        robot_ready = False
+
+    # APPLY UDP JOINTS
+    if (
+        robot_ready
+        and udp_receiver.latest_joint_positions is not None
+    ):
+
+        try:
+            robot.set_joint_positions(
+                udp_receiver.latest_joint_positions
+            )
+
+            print("APPLIED")
+
+        except Exception as e:
+            print("SET JOINT ERROR:", e)
 
 simulation_app.close()
